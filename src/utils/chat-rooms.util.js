999c50e995975c3031880009d6919130c006428a
platypusrex <frankcooke79@gmail.com>
2017-07-12 10:04:36 -0400

    adds constants for chat room socket events

--- src/utils/chat-rooms.util.js
+++ src/utils/chat-rooms.util.js
@@ -2,6 +2,19 @@ import IO from 'koa-socket';
 import { gameCategories } from '../seed/game-categories.seed';
 import Chat from '../models/chat.model';
 
+const chatRoomEvents = {
+	connection: 'connection',
+	joinRoom: 'joinRoom',
+	message: 'message',
+	newMessage: 'newMessage',
+	typing: 'typing',
+	userTyping: 'userTyping',
+	userStopTyping: 'userStopTyping',
+	stopTyping: 'stopTyping',
+	leaveRoom: 'leaveRoom',
+	disconnect: 'disconnect'
+};
+
 export function initializeChatRooms(app) {
 	const chatRooms = Object.keys(gameCategories);
 
@@ -10,42 +23,42 @@ export function initializeChatRooms(app) {
 	})
 	.forEach(chatRoom => {
 		chatRoom.roomName.attach(app);
-		chatRoom.roomName.on('connection', ctx => {
+		chatRoom.roomName.on(chatRoomEvents.connection, ctx => {
 			const socket = ctx.socket;
 			console.log('someone connect: %s', socket.id);
 
-			socket.on('joinRoom', ctx => {
-				socket.emit('message', {
+			socket.on(chatRoomEvents.joinRoom, ctx => {
+				socket.emit(chatRoomEvents.message, {
 					message: `Welcome to the ${ctx} chat room!`,
 					userName: 'dev-bot',
 					createdOn: new Date()
 				});
 			});
 
-			socket.on('newMessage', async ctx => {
+			socket.on(chatRoomEvents.newMessage, async ctx => {
 				const newChat = new Chat(ctx);
 				await newChat.save();
-				chatRoom.roomName.broadcast('message', ctx);
+				chatRoom.roomName.broadcast(chatRoomEvents.message, ctx);
 			});
 
-			socket.on('typing', ctx => {
-				chatRoom.roomName.broadcast('userTyping', {userName: ctx, isTyping: true});
+			socket.on(chatRoomEvents.typing, ctx => {
+				chatRoom.roomName.broadcast(chatRoomEvents.userTyping, {userName: ctx, isTyping: true});
 
 			});
 
-			socket.on('stopTyping', ctx => {
-				chatRoom.roomName.broadcast('userStopTyping', null);
+			socket.on(chatRoomEvents.stopTyping, ctx => {
+				chatRoom.roomName.broadcast(chatRoomEvents.userStopTyping, null);
 			});
 
-			socket.on('leaveRoom', ctx => {
-				chatRoom.roomName.broadcast('message', {
+			socket.on(chatRoomEvents.leaveRoom, ctx => {
+				chatRoom.roomName.broadcast(chatRoomEvents.message, {
 					message: `${ctx} has left the room.`,
 					userName: 'dev-bot',
 					createOn: new Date()
 				});
 			});
 
-			socket.on('disconnect', ctx => {
+			socket.on(chatRoomEvents.disconnect, ctx => {
 				console.log('user has left the room ' + ctx);
 			});
 		});
