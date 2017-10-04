86fff166c4b8c930180bff98a457d7de2fa46fea
platypusrex <frankcooke79@gmail.com>
2017-10-04 14:15:25 -0400

    Add - game room constants file

---
+++ src/constants/game-room.constants.js
@@ -0,0 +1,41 @@
+import { defaultSocketEvents } from "./socket-events.constants";
+
+export const gameStatus = {
+	pending: 'pending',
+	started: 'started',
+	cancelled: 'cancelled',
+	timedOut: 'timedOut'
+};
+
+export const gameType = {
+	onePlayer: 'onePlayer',
+	twoPlayer: 'twoPlayer'
+};
+
+export const playerStatus = {
+	joined: 'joined',
+	cancelled: 'cancelled'
+};
+
+const onePlayerGameEvents = {
+	createOnePlayerGame: 'createOnePlayerGame',
+	cancelOnePlayerGame: 'cancelOnePlayerGame',
+	onePlayerGameCreated: 'onePlayerGameCreated',
+	onePlayerGameCreatedSuccess: 'onePlayerGameCreatedSuccess',
+	onePlayerGameCanceled: 'onePlayerGameCanceled',
+	onePlayerGameEnded: 'onePlayerGameEndedSuccess'
+};
+
+const twoPlayerGameEvents = {
+	createTwoPlayerGame: 'createTwoPlayerGame',
+	cancelTwoPlayerGame: 'cancelTwoPlayerGame',
+	twoPlayerGameCreated: 'twoPlayerGameCreated',
+	twoPlayerGameCreatedSuccess: 'twoPlayerGameCreatedSuccess',
+	twoPlayerGameStarted: 'twoPlayerGameStarted',
+	twoPlayerGameStartedSuccess: 'twoPlayerGameStartedSuccess',
+	twoPlayerGameCanceled: 'twoPlayerGameCanceled',
+	twoPlayerGameEnded: 'twoPlayerGameEndedSuccess',
+	leaveTwoPlayerGameRoom: 'leaveTwoPlayerGameRoom'
+};
+
+export const gameRoomEvents = Object.assign({}, defaultSocketEvents, onePlayerGameEvents, twoPlayerGameEvents);
